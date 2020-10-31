b429c0dc03c5ee3ddc43a878cbc3da78446ee261
Shamil Garipov <garipov.workmail@gmail.com>
2020-10-31 09:01:01 +0300

    app.js modify

--- app.js
+++ app.js
@@ -9,8 +9,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 // говорим, что мы раздаем папку public
 app.use(express.static('public'));
 // require('./app/routes')(app, pool);
-const port = process.env.port || 3000;
+app.set('port', process.env.PORT || 3000);
 // говорим, что запускаемся на порту 80
-app.listen(port);
 
 console.log("Server started at 5000");
